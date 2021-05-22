aef496d11c3e3286246f4b61dd73cb90d8e2bfc9
Shreya-P-D <75159115+Shreya-P-D@users.noreply.github.com>
2021-05-22 12:15:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var database;
+var canvas,backgroundImage;
+var gameState=0;
+var playerCount;
+var database;
+var form,player,game;
+
+function setup(){
+  database = firebase.database();
+    createCanvas(500,500);
+  game=new Game()
+  game.getState()
+  game.start()
+ 
+}
+
+function draw(){
+  
+}
+
+
+
+
+
